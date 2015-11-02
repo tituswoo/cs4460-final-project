5c101afffcc8274866096d8ebbb7c7107886aa8e
Titus Woo <tituswoo@lawn-128-61-53-220.lawn.gatech.edu>
2015-11-02 16:54:50 -0500

    Configured webpack with necessary React stuff. Basic 'hello world' is working.

---
+++ index.jsx
@@ -0,0 +1,9 @@
+/** @jsx React.DOM */
+
+'use strict'
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Hello = require('./Hello')
+
+ReactDOM.render(<Hello />, document.getElementById('content'))
