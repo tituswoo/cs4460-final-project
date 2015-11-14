05c66ac7a5632142cfcc9b7fad56e056b8deb6f2
Titus Woo <titus.woo@me.com>
2015-11-14 02:33:24 -0500

    Map zooms in on the user's current gps location

--- SimpleMap.jsx
+++ SimpleMap.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import {GoogleMap, Marker} from 'react-google-maps';
 
 class SimpleMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLocation: {
+        lat: 0,
+        lng: 0
+      }
+    };
+  }
+
   render() {
     return (
       <GoogleMap containerProps={{
           className: 'SimpleMap'
         }}
-        defaultZoom={3}
-        defaultCenter={{lat: -25.363882, lng: 131.044922}}
+        defaultZoom={12}
+        center={this.state.currentLocation}
         onClick={this.props.onMapClick}
       >
         {this.props.markers.map((marker, index) => {
@@ -20,6 +30,18 @@ class SimpleMap extends React.Component {
         })}
       </GoogleMap>
     );
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      console.log(location);
+      this.setState({
+        currentLocation: {
+          lat: location.coords.latitude,
+          lng: location.coords.longitude
+        }
+      });
+    }.bind(this));
   }
 
   onMapClick() {
