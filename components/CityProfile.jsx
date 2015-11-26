eb3dcfdfda1cb8bd682eb1e88b6015bccff5874e
Titus Woo <titus.woo@me.com>
2015-11-26 18:08:21 -0500

    shows summary for safety, health care, and qualtiy of life.

--- components/CityProfile.jsx
+++ components/CityProfile.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import {GoogleMapLoader, GoogleMap, Marker} from 'react-google-maps';
 import MapPreview from '../components/MapPreview';
+import EmoticonReportTile from '../components/EmoticonReportTile';
 
 class CityProfile extends React.Component {
     render() {
@@ -10,7 +11,7 @@ class CityProfile extends React.Component {
       if (!this.props.city || !this.props.city.indices) {
         return (<div />);
       }
-      console.log(this.props.cityMeta);
+      console.log(this.props.city.indices);
       return (
         <div className='city-profile'>
           <MapPreview
@@ -18,10 +19,93 @@ class CityProfile extends React.Component {
             subtitle={this.props.cityMeta.country}
             latitude={this.props.cityMeta.latitude}
             longitude={this.props.cityMeta.longitude} />
-          <p><strong>Safety:</strong>{this.props.city.indices.safety_index}</p>
+          <EmoticonReportTile
+            title='Safety'
+            rating={this.props.city.indices.safety_index}
+            scale={[
+              {
+                from: 0, to: 20,
+                remark: 'Very low',
+                icon: 'fa-frown-o'
+              },
+              {
+                from: 20, to: 40,
+                remark: 'Low',
+                icon: 'fa-frown-o'
+              },
+              {
+                from: 40, to: 55,
+                remark: 'Moderate',
+                icon: 'fa-meh-o'
+              },
+              {
+                from: 60, to: 80,
+                remark: 'High',
+                icon: 'fa-smile-o'
+              },
+              {
+                from: 80, to: 100,
+                remark: 'Very high',
+                icon: 'fa-smile-o'
+              }
+            ]}/>
+          <EmoticonReportTile
+            title='Health Care'
+            rating={this.props.city.indices.health_care_index}
+            scale={[
+              {
+                from: 0, to: 40,
+                remark: 'Low',
+                icon: 'fa-frown-o'
+              },
+              {
+                from: 40, to: 60,
+                remark: 'Moderate',
+                icon: 'fa-meh-o'
+              },
+              {
+                from: 60, to: 80,
+                remark: 'High',
+                icon: 'fa-smile-o'
+              },
+              {
+                from: 80, to: 100,
+                remark: 'Very high',
+                icon: 'fa-smile-o'
+              }
+            ]}/>
+          <EmoticonReportTile
+            title='Quality of Life'
+            rating={this.props.city.indices.quality_of_life_index}
+            scale={[
+              {
+                from: 0, to: 35,
+                remark: 'Very low',
+                icon: 'fa-frown-o'
+              },
+              {
+                from: 35, to: 65,
+                remark: 'Low',
+                icon: 'fa-frown-o'
+              },
+              {
+                from: 65, to: 100,
+                remark: 'Medium',
+                icon: 'fa-meh-o'
+              },
+              {
+                from: 100, to: 135,
+                remark: 'High',
+                icon: 'fa-smile-o'
+              },
+              {
+                from: 135, to: 200,
+                remark: 'Very high',
+                icon: 'fa-smile-o'
+              }
+            ]}/>
+
           <p><strong>Average time in traffic:</strong>{this.props.city.indices.traffic_index} min</p>
-          <p><strong>Happiness:</strong>{this.props.city.indices.quality_of_life_index}</p>
-          <p><strong>Pollution:</strong>{this.props.city.indices.pollution_index}</p>
           <p><strong>Purchasing power:</strong>{this.props.city.indices.purchasing_power_incl_rent_index}</p>
           <p><strong>Cost of living:</strong>{this.props.city.indices.cpi_index}</p>
         </div>
---
+++ components/EmoticonReportTile.jsx
@@ -0,0 +1,63 @@
+'use strict';
+
+import React from 'react';
+
+class EmoticonReportTile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className='emoticon-report-tile'>
+        <i className={'fa ' + this.state.icon + ' emoticon-report-tile__emote'}></i>
+        <div>
+          <h3>{this.props.title}</h3>
+          <p>{this.state.remark}</p>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this._pickRemarkAndEmote(this.props.scale, this.props.rating);
+  }
+
+  _pickRemarkAndEmote(scale, rating) {
+    if (scale.length < 1) {
+      this.setState({
+        remark: 'No scale defined!',
+        icon: 'fa-question-circle'
+      });
+    } else {
+      for (let i = 0; i < scale.length; i++) {
+        if (rating >= scale[i].from && rating <= scale[i].to) {
+          this.setState({
+            remark: scale[i].remark + '.',
+            icon: scale[i].icon
+          });
+          return;
+        }
+      }
+      this.setState({
+        remark: 'Unknown',
+        icon: 'fa-question-circle'
+      });
+    }
+  }
+}
+
+EmoticonReportTile.propTypes = {
+  title: React.PropTypes.string,
+  rating: React.PropTypes.number,
+  scale: React.PropTypes.array
+};
+
+EmoticonReportTile.defaultProps = {
+  title: 'Title here',
+  rating: 0,
+  scale: []
+};
+
+export default EmoticonReportTile;
