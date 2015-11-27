5d23ba7d857e62af547d5aaaaff5cb56167d989d
Titus Woo <titus.woo@me.com>
2015-11-27 02:17:52 -0500

    styles for loading dialog

--- components/LoadingDialog.jsx
+++ components/LoadingDialog.jsx
@@ -5,8 +5,10 @@ import React from 'react';
 class LoadingDialog extends React.Component {
   render() {
     return (
-      <div>
-        <h1><i className='fa fa-spin fa-circle-o-notch'></i> Loading...</h1>
+      <div className='loading-dialog'>
+        <h1>
+          <i className='fa fa-spin fa-circle-o-notch'></i> Loading..
+          </h1>
       </div>
     );
   }
